29c25b25be87486785b8e0107c820bdd1e7c6e83
Charlotte <81417641+Cuzozzi@users.noreply.github.com>
2022-06-26 11:38:57 +0200

    type orm

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "<3tomas",
+  database: "project-omerta-server",
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
