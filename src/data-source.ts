78d1243d540cb777a9a63b2f5313f287ee75c5bd
Charlotte <81417641+Cuzozzi@users.noreply.github.com>
2022-06-26 17:53:54 +0200

    postgres/express/typeorm install

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   port: 5432,
   username: "postgres",
   password: "<3tomas",
-  database: "project-omerta-server",
+  database: "postgres",
   synchronize: true,
   logging: false,
   entities: [User],
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,33 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
+const express = require("express");
+const app = express();
+const port = 5433;
 
-AppDataSource.initialize().then(async () => {
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await AppDataSource.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    console.log("Loading users from the database...");
+    const users = await AppDataSource.manager.find(User);
+    console.log("Loaded users: ", users);
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+    console.log(
+      "Here you can setup and run express / fastify / any other framework."
+    );
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
 
-}).catch(error => console.log(error))
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
