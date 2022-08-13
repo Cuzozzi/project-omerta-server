4a1a5dbaae5422546bee0173b1325a596c941a99
Cuzozzi <cuzozzi00@gmail.com>
2022-08-13 17:29:10 +0200

    Tile Generation Finished

--- src/routes/map.ts
+++ src/routes/map.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { AppDataSource } from "../data-source";
+import { ReturnOne } from "../helpers/map-returns";
+
 let router = express.Router();
 
 //count all tiles
@@ -51,84 +53,15 @@ router.get("/tile-generation", async (req, res) => {
   );
   const lastTile = allTiles[allTiles.length - 1];
   console.log(allTiles);
-  console.log(lastTile);
   console.log(lastTile.x, lastTile.z);
   const x = Number(lastTile.x);
   const z = Number(lastTile.z);
 
-  AppDataSource.manager
-    .query(`SELECT * FROM tile_positions WHERE x = ${x - 32} AND z = ${z}`)
-    .then(async (response) => {
-      if (response.length > 0) {
-        console.log("Return One");
-        res.status(200).json({ response: response, x: x, z: z + 32 });
-        AppDataSource.manager.query(
-          `LOCK TABLE tile_positions; INSERT INTO tile_positions (x, y, z) SELECT ${x}, 0, ${
-            z + 32
-          } WHERE NOT EXISTS (SELECT 1 FROM tile_positions WHERE z = ${
-            z + 32
-          } AND x = ${x} AND y = 0)`
-        );
-        return;
-      }
-      AppDataSource.manager
-        .query(`SELECT * FROM tile_positions WHERE x = ${x} AND z = ${z - 32}`)
-        .then(async (response) => {
-          if (response.length > 0) {
-            res.status(200).json({ response: response, x: x - 32, z: z });
-            console.log("Return Two");
-            AppDataSource.manager.query(
-              `LOCK TABLE tile_positions; INSERT INTO tile_positions (x, y, z) SELECT ${
-                x - 32
-              }, 0, ${z} WHERE NOT EXISTS (SELECT 1 FROM tile_positions WHERE z = ${z} AND x = ${
-                x - 32
-              } AND y = 0)`
-            );
-            return;
-          }
-          AppDataSource.manager
-            .query(
-              `SELECT * FROM tile_positions WHERE x = ${x + 32} and z = ${z}`
-            )
-            .then(async (response) => {
-              if (response.length > 0) {
-                res.status(200).json({ resppnse: response, x: x, z: z - 32 });
-                console.log("Return Three");
-                AppDataSource.manager.query(
-                  `LOCK TABLE tile_positions; INSERT INTO tile_positions (x, y, z) SELECT ${x}, 0, ${
-                    z - 32
-                  } WHERE NOT EXISTS (SELECT 1 FROM tile_positions WHERE z = ${
-                    z - 32
-                  } AND x = ${x} AND y = 0)`
-                );
-                return;
-              }
-              AppDataSource.manager
-                .query(
-                  `SELECT * FROM tile_positions WHERE x = ${x} and z = ${
-                    z + 32
-                  }`
-                )
-                .then(async (response) => {
-                  if (response.length > 0) {
-                    res
-                      .status(200)
-                      .json({ response: response, x: x + 32, z: z });
-                    console.log("Return Four");
-                    AppDataSource.manager.query(
-                      `LOCK TABLE tile_positions; INSERT INTO tile_positions (x, y, z) SELECT ${
-                        x + 32
-                      }, 0, ${z} WHERE NOT EXISTS (SELECT 1 FROM tile_positions WHERE z = ${z} AND x = ${
-                        x + 32
-                      } AND y = 0)`
-                    );
-                    return;
-                  }
-                  res.sendStatus(401);
-                });
-            });
-        });
-    });
+  if (allTiles) {
+    ReturnOne(x, z, res);
+  } else {
+    res.sendStatus(500);
+  }
 });
 
 export default router;
